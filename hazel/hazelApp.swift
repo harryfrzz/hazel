b90eab76e3e9f74daad676725468785d3435384d
harryfrzz <harikrishnac005@gmail.com>
2026-03-30 12:46:18 +0530

    Initial Commit

---
+++ hazel/hazelApp.swift
@@ -0,0 +1,17 @@
+//
+//  hazelApp.swift
+//  hazel
+//
+//  Created by Harikrishna C on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct hazelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
